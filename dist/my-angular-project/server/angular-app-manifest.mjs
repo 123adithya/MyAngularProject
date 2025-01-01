23168afeb75df0f5888077802fb7a91799868269
Adithya Abburi <adithyaabburi@Adithyas-MacBook-Air.local>
2025-01-02 01:26:34 +0530

    feat(components): github-opages-1, fixed

--- dist/my-angular-project/server/angular-app-manifest.mjs
+++ dist/my-angular-project/server/angular-app-manifest.mjs
@@ -7,23 +7,23 @@ export default {
   routes: [
   {
     "renderMode": 2,
-    "redirectTo": "/counter",
-    "route": "/"
+    "redirectTo": "/MyAngularProject/counter",
+    "route": "/MyAngularProject"
   },
   {
     "renderMode": 2,
-    "route": "/counter"
+    "route": "/MyAngularProject/counter"
   },
   {
     "renderMode": 2,
-    "route": "/vatavaran"
+    "route": "/MyAngularProject/vatavaran"
   }
 ],
   assets: {
-    'index.csr.html': {size: 4965, hash: 'beb9d1d96c88de204f975c70ed7c4cc1ba4061e3be0eeb60405382342dfafd13', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1064, hash: 'b5c43f52f27b1290ed6c06869de6e12f4e4a99a5be6af208544830902a4d76d5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'counter/index.html': {size: 13859, hash: '9e483dabde48aef67dcea0c648868575c5a5e5475d76fbc027a144f49ef8e297', text: () => import('./assets-chunks/counter_index_html.mjs').then(m => m.default)},
-    'vatavaran/index.html': {size: 18830, hash: 'c0fa663bf38d6a37304e5d13e7e9049076dedfd1a434869860ff097cf4a74358', text: () => import('./assets-chunks/vatavaran_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 4982, hash: 'c7877cfe4d3e717829dfabe58df7141ef54542736fecd5f9a45ab0c9c00b5130', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1081, hash: '4b7841cf26799869614ce72a7bdb6d331cb106e046fde0017933fec6fd645f06', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'MyAngularProject/vatavaran/index.html': {size: 18881, hash: '75512ca71cc0d4e9e7dc8fbaa903c7d788e27436e812a9cda2a414992879069d', text: () => import('./assets-chunks/MyAngularProject_vatavaran_index_html.mjs').then(m => m.default)},
+    'MyAngularProject/counter/index.html': {size: 13910, hash: '190517b9f3017907d04239f762cc55c65a2dd082cba010aebdafe746e6a66192', text: () => import('./assets-chunks/MyAngularProject_counter_index_html.mjs').then(m => m.default)},
     'styles-DZ6UBGXD.css': {size: 231612, hash: 'B2Fy9V+bfZo', text: () => import('./assets-chunks/styles-DZ6UBGXD_css.mjs').then(m => m.default)}
   },
 };
--- dist/my-angular-project/server/assets-chunks/index_server_html.mjs
+++ dist/my-angular-project/server/assets-chunks/index_server_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>MyAngularProject</title>
-  <base href="/">
+  <base href="/MyAngularProject/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" href="styles-DZ6UBGXD.css"></head>
